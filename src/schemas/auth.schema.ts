a708daa13ff9f1917b2df81c87efa3abae88cd68
cherryjayhawk <muhbintangr19@gmail.com>
2025-11-13 14:11:19 +0700

    define auth schema

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,24 @@
+const registerSchema = {
+  body: {
+    type: 'object',
+    required: ['email', 'password', 'name'],
+    properties: {
+      email: { type: 'string', format: 'email' },
+      password: { type: 'string', minLength: 6 },
+      name: { type: 'string', minLength: 3 },
+    },
+  },
+};
+
+const loginSchema = {
+  body: {
+    type: 'object',
+    required: ['email', 'password'],
+    properties: {
+      email: { type: 'string', format: 'email' },
+      password: { type: 'string' },
+    },
+  },
+};
+
+export { registerSchema, loginSchema };
